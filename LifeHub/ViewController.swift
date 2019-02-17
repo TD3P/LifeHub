d77647af2b09bc4d361505729dd559e18f37bdd1
TD3P <10195428+TD3P@users.noreply.github.com>
2019-02-17 20:28:39 +0900

    Initial Commit

---
+++ LifeHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeHub
+//
+//  Created by 三瓶士継 on 2019/02/17.
+//  Copyright © 2019 TD3P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
